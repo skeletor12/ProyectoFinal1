f83106c56d179715ab66e6195c606af4d9d51659
luisrodriguez <betowas12345@hotmail.com>
2016-09-28 23:44:18 -0500

    Initial Commit

---
+++ ProyectoFinal1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ProyectoFinal1
+//
+//  Created by Luis Rodriguez on 28/09/16.
+//  Copyright © 2016 Luis Rodriguez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
